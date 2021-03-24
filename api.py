090ec8e1c9e71ab47c8c71da0b7cd9e42d3e1fc7
Barbara Asiamah <barbaraasiamah99@gmail.com>
2021-03-24 15:57:31 +0000

    update

--- api.py
+++ api.py
@@ -112,7 +112,7 @@ def startRegionsResponse(update, context):
     setStage(user, "start.number_of_regions")
     
         
-    context.bot.send_message(chat_id=update.effective_chat.id, text="The regions in Ghana are {}.".format(*regions))
+    context.bot.send_message(chat_id=update.effective_chat.id, text="The regions in Ghana are {}.".format(i for i in regions))
     stop(update, context)
     
 def message(update, context):
