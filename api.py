fa02f79c21b001c4be206cfb5ca589598154ccad
Barbara Asiamah <barbaraasiamah99@gmail.com>
2021-04-06 00:08:33 +0100

    update

--- api.py
+++ api.py
@@ -57,7 +57,7 @@ for x in constituencies_data:
     key= x['regionCode']
     value = x['name']
     
-    if key not in groups :
+    if value not in groups :
         groups.update({key: [value] })        
 print(groups)
 # for d in constituencies_data:
