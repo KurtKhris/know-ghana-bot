cb1f30d60f5f5fa3edf2dd2885ea768f2d6c076f
Barbara Asiamah <barbaraasiamah99@gmail.com>
2021-04-05 18:17:13 +0100

    update regions

--- api.py
+++ api.py
@@ -120,13 +120,12 @@ def startRegionsResponse(update, context):
     user = update.effective_user.id
     setStage(user, "start.regions")
     context.bot.send_message(chat_id=update.effective_chat.id, text="The regions in Ghana are: ")
-    stop(update, context)
-
+    
     for i in regions:
         if len(regions) == len(regions):
             context.bot.send_message(chat_id=update.effective_chat.id, text=i)
             
-    #stop(update, context)
+    stop(update, context)
     
 def startNumberConstituenciesResponse(update, context):
     user = update.effective_user.id
@@ -140,12 +139,13 @@ def startConstituenciesResponse(update, context):
     user = update.effective_user.id
     setStage(user, "start.constituencies")
     context.bot.send_message(chat_id=update.effective_chat.id, text="The constituencies in Ghana are: ")
-    
+    stop(update, context)
+
     for c in constituencies:
         if len(constituencies) == len(constituencies):
             context.bot.send_message(chat_id=update.effective_chat.id, text=c)
             
-    stop(update, context)
+    #stop(update, context)
     
 def message(update, context):
     user = update.effective_user.id
